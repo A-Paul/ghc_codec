74690506178024f9dda75e0fbd1c4b6eb5a478c4
Andreas "Paul" Pauli <a.pauli@digimint.de>
2018-07-04 05:36:15 +0200

    ghc_decompress + test are functional

--- Makefile
+++ Makefile
@@ -1,18 +1,21 @@
 CC = $(shell which gcc)
 
 CFLAGS_INCLUDES = -I./include
-CFLAGS_DEBUG = -g
-CFLAGS_CC = -strict -std=c11 -Wall
-CFLAGS += $(CFLAGS_CC) $(CFLAGS_INCLUDES)
+CFLAGS_DEBUG = -g -DDEBUG=1
+CFLAGS_CC = -std=c11 -Wall
+CFLAGS_EX = -pedantic -Wextra $(CFLAGS_DEBUG)
+CFLAGS += $(CFLAGS_CC) $(CFLAGS_INCLUDES) $(CFLAGS_EX)
 
 TARGETS = check_ghc_codec
-OBJECT_check_ghc_codec = $(TARGETS:%=%.o)
+SOURCES_check_ghc_codec = check_ghc_codec.c src/ghc_codec.c
+OBJECTS_check_ghc_codec = $(SOURCES_check_ghc_codec:%.c=%.o)
 
 LDFLAGS +=
 
 all: $(TARGETS)
 
-check_ghc_codec: $(OBJECTS_check_ghc_codec) 
+check_ghc_codec: $(OBJECTS_check_ghc_codec)
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $^
 
 clean:
-	$(RM) $(TARGETS) $(OBJECTS)
+	$(RM) $(TARGETS) $(OBJECTS_check_ghc_codec)
---
+++ include/ghc_codec.h
@@ -0,0 +1,40 @@
+#ifndef GHC_CODEC_H
+#define GHC_CODEC_H
+
+#include "stdint.h"
+
+/*
+ * Draft for one/two struct(s) carrying the buffers and meta data
+ * of (de)compressor.
+ *
+ */
+struct ghc_coder {
+  /* uint32_t status; */       /*  */
+  uint8_t* uncompressed;       /* source */
+  uint8_t* compressed;         /* target */
+  size_t   size_unco;	       /*  */
+  size_t   size_comp;	       /*  */
+  uint16_t pos_unco;	       /*  */
+  uint16_t pos_comp;	       /*  */
+  uint8_t  na;		       /*  */
+  uint8_t  sa;		       /*  */
+};
+
+/*!
+ * \param[in]    source uncompressed byte array.
+ * \param[in]    sn number of elements in source.
+ * \param[inout] target compressed byte array.
+ * \param[inout] tn number of elements in target.
+ */
+int ghc_compress (uint8_t* source, size_t sn, uint8_t* target, size_t tn);
+
+
+/*!
+ * \param[in]    source compressed bytecode.
+ * \param[in]    sn number of elements in source.
+ * \param[inout] target uncompressed byte array.
+ * \param[inout] tn number of elements in target.
+ */
+int ghc_decompress (uint8_t* source, size_t sn, uint8_t* target, size_t tn);
+
+#endif /* GHC_CODEC_H */
